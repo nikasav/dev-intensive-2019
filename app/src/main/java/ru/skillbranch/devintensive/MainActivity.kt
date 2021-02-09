872d8618e8a262160542ea458bde980bfa76c8fe
Veronika <veronica.saveleva@gmail.com>
2021-02-09 21:11:39 +0300

    Урок 3 основные задания

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,11 +1,76 @@
-package ru.skillbranch.devintensive
+    package ru.skillbranch.devintensive
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Context
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import ru.skillbranch.devintensive.models.Bender
 
-class MainActivity : AppCompatActivity() {
+    class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var benderImage: ImageView
+    lateinit var textTxt: TextView
+    lateinit var messageEt: EditText
+    lateinit var sendBtn: ImageView
+
+    lateinit var benderObj: Bender
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        benderImage     = findViewById(R.id.iv_bender)
+        textTxt         = findViewById(R.id.tv_text)
+        messageEt       = findViewById(R.id.et_message)
+        sendBtn         = findViewById(R.id.iv_send)
+
+        val status = savedInstanceState?.getString("STATUS")?:Bender.Status.NORMAL.name
+        val question = savedInstanceState?.getString("QUESTION")?:Bender.Question.NAME.name
+
+        benderObj = Bender(Bender.Status.valueOf(status), Bender.Question.valueOf(question))
+
+        val (r, g, b) = benderObj.status.color
+        benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
+
+        messageEt.setText(savedInstanceState?.getString("ET_MESSAGE") ?: "")
+        textTxt.text = benderObj.askQuestion()
+        sendBtn.setOnClickListener(this)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("STATUS", benderObj.status.name)
+        outState.putString("QUESTION", benderObj.question.name)
+        outState.putString("ET_MESSAGE", messageEt.text.toString())
+        Log.d("M_MainActivity", "onSaveInstanceState ${benderObj.status.name}   ${benderObj.question.name} ")
+    }
+
+    override fun onClick(v: View?) {
+        if(v?.id == R.id.iv_send || v?.id == EditorInfo.IME_ACTION_DONE){
+            val (phrase, color) = benderObj.listenAnswer(messageEt.text.toString().toLowerCase())
+            messageEt.setText("")
+            val (r, g, b) = color
+            benderImage.setColorFilter(Color.rgb(r, g, b), PorterDuff.Mode.MULTIPLY)
+            textTxt.text = phrase
+            textTxt.clearFocus()
+            hideKeyboard(this)
+        }
+    }
+
+    fun hideKeyboard(activity: Activity) {
+        val view = activity.findViewById<View>(android.R.id.content)
+        if (view != null) {
+            val imm: InputMethodManager = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 }
